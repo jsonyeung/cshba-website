89c8debc332510e4c50875d97e2158a93b703d06
Jason Yeung <jsy9@sfu.ca>
2017-08-13 19:24:25 -0700

    update nav breakpoints & mobile functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-    $nav = $('.m-nav');
+    var $nav     = $('.m-nav');
+    var $hamburg = $('.m-nav__hamburger');
 
-    $hamburg = $('.m-nav__hamburger');
+    var $mob_nav = $('.m-nav__menu--mobile');
+    var $overlay = $('.m-nav__overlay');
 
-    $hamburg.click(function() {
-        $('.m-nav__menu--mobile').toggleClass('m-nav__menu--active');
-        $('.m-nav__overlay').toggleClass('m-nav__overlay--active');
-    });
+    var open = false;
+
+    TweenLite.set($mob_nav, { x: 500 });
+
+    var toggle_nav = function() {
+        if (open) {
+            TweenLite.to($overlay, 0.05, { autoAlpha: 0 });
+            TweenLite.to($mob_nav, 0.5, { x:500, ease: Power1.easeOut });
+        } else {
+            TweenLite.to($overlay, 0.05, { autoAlpha: 1 });
+            TweenLite.to($mob_nav, 0.5, { x:0, ease: Power1.easeOut });
+        }
+        open = !open;
+    };
+
+    $hamburg.click(toggle_nav);
+    $overlay.click(function() { toggle_nav(); });
 });
