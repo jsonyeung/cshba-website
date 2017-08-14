c5b30d32a9a917ae0d7f7337741b25be9d89c42d
Jason Yeung <jsy9@sfu.ca>
2017-08-13 17:45:15 -0700

    mobile nav open/close functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-alert("Test")
+$(document).ready(function() {
+    $nav = $('.m-nav');
+
+    $hamburg = $('.m-nav__hamburger');
+
+    $hamburg.click(function() {
+        $('.m-nav__menu--mobile').toggleClass('m-nav__menu--active');
+        $('.m-nav__overlay').toggleClass('m-nav__overlay--active');
+    });
+});
